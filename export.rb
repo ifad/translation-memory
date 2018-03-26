6583b7efa622819dd7034f2093ccf3c3fffe4308
Marcello Barnaba <vjt@openssl.it>
2018-03-26 16:27:51 +0200

    More flexible exporter

--- export.rb
+++ export.rb
@@ -10,34 +10,24 @@ end
 
 Pontoon.connect!
 
-project = Pontoon::Project.lookup(project_slug)
-unless project
+@project = Pontoon::Project.lookup(project_slug)
+unless @project
   puts "Project `#{project_slug}' was not found"
   exit 2
 end
 
-locale = project.locales.lookup(locale)
-unless locale
+@locale = @project.locales.lookup(locale)
+unless @locale
   puts "Project `#{project_slug}` is not translated to `#{locale}`"
   exit 3
 end
 
-Pontoon.cheer "Please call ret() passing the translations you want to export"
-
-catch :transl do
-  def ret(translations)
-    @translations = translations
-    throw :transl
-  end
-
-  Translations = project.translations.by_locale(locale)
-
-  binding.pry
+def export(translations)
+  Pontoon.export_translations translations, @project, @locale
 end
 
-unless @translations
-  puts "No translations found to export."
-  exit 4
-end
+Pontoon.cheer "Call export() passing the translations you want to get."
 
-Pontoon.export_translations @translations, project, locale
+Scope = @project.translations.by_locale(@locale)
+
+binding.pry
